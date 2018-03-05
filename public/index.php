d574f7ae0d89349d0215d21a7098daac8798ec59
wangxiaochi <wangxiaochi@tuhu.cn>
2018-03-05 22:24:51 +0800

    first commit

---
+++ public/index.php
@@ -0,0 +1,20 @@
+<?php
+
+use Lib\RegexRouter;
+
+define("ROOT", dirname(__DIR__));
+include ROOT.'/lib/lib.php';
+autoload_dir("Lib", ROOT.'/lib');
+env_load(ROOT);
+
+RegexRouter::add('#/subject/(\d+)/#', function ($params) {
+    $id = $params[1];
+    $file = ROOT.'/_movie/'.$id;
+    readfile($file);
+}, 'GET');
+RegexRouter::add('#/j/review/(\d+)/full#', function ($params) {
+    $id = $params[1];
+    $file = ROOT.'/_j_review/'.$id;
+    readfile($file);
+}, 'GET');
+RegexRouter::run();
